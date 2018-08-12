f48c17ef2c233ea47bde8f61f232289a57ff46a7
Yamit Villamil <yamit.villamil@globant.com>
2018-08-12 17:29:42 -0500

    Redirects

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -50195,13 +50195,17 @@ var Router = require("react-router");
 var DefaultRoute = Router.DefaultRoute;
 var Route = Router.Route;
 var NotFoundRoute = Router.NotFoundRoute;
+var Redirect = Router.Redirect;
 
 var routes = (
     React.createElement(Route, {name: "app", path: "/", handler: require("./components/app")}, 
         React.createElement(DefaultRoute, {handler: require("./components/homePage")}), 
         React.createElement(Route, {name: "authors", handler: require("./components/authors/authorPage")}), 
         React.createElement(Route, {name: "about", handler: require("./components/about/aboutPage")}), 
-        React.createElement(NotFoundRoute, {handler: require("./components/notFoundPage")})
+        React.createElement(NotFoundRoute, {handler: require("./components/notFoundPage")}), 
+        React.createElement(Redirect, {from: "about-us", to: "about"}), 
+        React.createElement(Redirect, {from: "awthurs", to: "authors"}), 
+        React.createElement(Redirect, {from: "about/*", to: "about"})
     )
 );
 
