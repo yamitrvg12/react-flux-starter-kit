adeb65b85f94b897b871093f9cece082e3d60831
Yamit Villamil <yamit.villamil@globant.com>
2018-08-12 21:56:52 -0500

    Programmatic Redirects

--- dist/scripts/bundle.js
+++ dist/scripts/bundle.js
@@ -50160,10 +50160,15 @@ module.exports = AuthorPage;
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
+    mixins: [
+        Router.Navigation
+    ],
+
     getInitialState: function() {
         return {
             author: {
@@ -50186,6 +50191,7 @@ var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
     saveAuthor: function(event) {
         event.preventDefault();
         AuthorApi.saveAuthor(this.state.author);
+        this.transitionTo('authors');
     },
 
     render: function() {
@@ -50202,7 +50208,7 @@ var ManageAuthorPage = React.createClass({displayName: "ManageAuthorPage",
 
 module.exports = ManageAuthorPage;
 
-},{"../../api/authorApi":201,"./authorForm":205,"react":200}],209:[function(require,module,exports){
+},{"../../api/authorApi":201,"./authorForm":205,"react":200,"react-router":30}],209:[function(require,module,exports){
 "use strict";
 
 var React = require('react');
--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -1,10 +1,15 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
 
 var ManageAuthorPage = React.createClass({
+    mixins: [
+        Router.Navigation
+    ],
+
     getInitialState: function() {
         return {
             author: {
@@ -27,6 +32,7 @@ var ManageAuthorPage = React.createClass({
     saveAuthor: function(event) {
         event.preventDefault();
         AuthorApi.saveAuthor(this.state.author);
+        this.transitionTo('authors');
     },
 
     render: function() {
